93533736f690256ab62f56599d2ec35045b633d4
Zofia Quillet <67431758+ZofiaQlt@users.noreply.github.com>
2024-09-20 22:18:58 +0200

    Update app_german.py

--- app_german.py
+++ app_german.py
@@ -211,7 +211,7 @@ def main():
 # CSS pour le formulaire de statistiques
     css = """
     <style>
-        [data-testid="stForm"] {
+        [data-testid="stats_form"] {
             background: #ffffe0;  /* Fond jaune pour le formulaire des statistiques */
         }
     </style>
