a0acd3ba704c272c076171440b2bfaace60de99b
Zofia Quillet <67431758+ZofiaQlt@users.noreply.github.com>
2024-09-20 22:34:44 +0200

    Update app_german.py

--- app_german.py
+++ app_german.py
@@ -206,20 +206,6 @@ def main():
     with st.form(key='stats_form'):
         if st.form_submit_button("Statistiques"):
             show_statistics()
-        # Ajoute ce CSS à la fin de ton code
- # CSS pour les formulaires
-    css = """
-    <style>
-        [data-testid="stForm"] {
-            background: white; /* Par défaut, le fond est blanc */
-        }
-        [data-testid="stForm"][key='stats_form'] {
-            background: #ffffe0; /* Pour le formulaire des statistiques */
-        }
-    </style>
-    """
-    st.write(css, unsafe_allow_html=True)
-
 
     
 if __name__ == "__main__":
