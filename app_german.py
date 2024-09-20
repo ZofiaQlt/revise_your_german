730e68bb2aad519bcab35ddec75b05b871053a66
Zofia Quillet <67431758+ZofiaQlt@users.noreply.github.com>
2024-09-20 13:32:03 +0200

    Update app_german.py

--- app_german.py
+++ app_german.py
@@ -111,7 +111,7 @@ def show_statistics():
             top_5_errors = sorted_errors[:5]
             st.write("---\n__Top 5 des mots avec le plus grand nombre d'erreurs :__\n")
             for word, count in top_5_errors:
-                error_label = "erreur" si count == 1 else "erreurs"
+                error_label = "erreur" if count == 1 else "erreurs"
                 st.write(f"- {word} : {count} {error_label}")
         else:
             st.write("Aucune erreur enregistrée.")
